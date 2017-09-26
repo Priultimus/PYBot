e43e8bf61e01bd401ba1f4cc6af0aadd8adc3bea
h0r1zon <libertsxion@gmail.com>
2017-09-26 18:42:34 -0400

    oops

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import logging 
 
 logger = logging.getLogger('discord')
-client = commands.Bot(command_prefix=commands.when_mentioned_or("!"), description=desc, pm_help=None)
+client = commands.Bot(command_prefix=commands.when_mentioned_or("!"), description="test", pm_help=None)
 
 @client.event
 async def on_ready():
