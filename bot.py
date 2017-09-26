5fae70172ce6ea3fe4509a3c69736494851b725b
Luke Taylor <luke@bigring.org>
2017-09-26 19:14:02 -0400

    Fixed Commands

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-import logging 
+import logging
 
 logger = logging.getLogger('discord')
 client = commands.Bot(command_prefix=commands.when_mentioned_or("!"), description="test", pm_help=None)
@@ -9,8 +9,8 @@ client = commands.Bot(command_prefix=commands.when_mentioned_or("!"), descriptio
 async def on_ready():
     print("Bot Online!")
 
-@commands.command()
-async def ping(self, ctx):
-    await ctx.send("Pong")
+@client.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
 
-client.run("Don't include the token when you commit.")
+client.run("token")
