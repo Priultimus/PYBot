5d13f8eeb38036025d308b48bf4a5d6a7ac78b51
Luke Taylor <luke@bigring.org>
2017-09-27 18:44:44 -0400

    Updated Serverinfo Command
    Added Server Creation Date

--- bot.py
+++ bot.py
@@ -1,9 +1,17 @@
 import discord
 from discord.ext import commands
 import logging
+import json
+import datetime
 
 logger = logging.getLogger('discord')
 client = commands.Bot(command_prefix=commands.when_mentioned_or("!"), description="test", pm_help=None)
+
+def snowflake_time(id):
+    time = datetime.datetime.utcfromtimestamp(((int(id) >> 22) + 1420070400000) / 1000)
+    times = "%s" %time
+    timesp = times.split('.', 1)
+    return timesp[0]
 
 @client.event
 async def on_ready():
@@ -18,10 +26,11 @@ async def serverinfo(ctx):
     em = discord.Embed(title='**Server Info**', colour=0xDEAFBF)
     em.add_field(name="Human Count", value="%s humans" %sum(not member.bot for member in ctx.guild.members), inline=True)
     em.add_field(name="Bot Count", value="%s bots" %sum(member.bot for member in ctx.guild.members), inline=True)
+    em.add_field(name="Server Creation Date", value="%s" %snowflake_time(ctx.guild.id), inline=False)
     await ctx.send(embed=em)
 
 @client.command()
 async def debug(ctx):
-    print(sum(not member.bot for member in ctx.guild.members))
+    print("Debug Message Goes Here")
 
 client.run("token")
